2990638522ad0ec12aa898289dce977fdf2d2a38
daniel-skillsbox <daniel.mugisha@skillsbox.com>
2026-02-20 16:32:50 +0200

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,11 @@ import './App.css'
 import { Header } from './components/Header'
 import { Hero } from './components/Hero'
 import { Showcase } from './components/Showcase'
-import { Clients } from './components/Clients'
 import { About } from './components/About'
 import { Strategies } from './components/Strategies'
 import { Features } from './components/Features'
-import { Portfolio } from './components/Portfolio'
 import { Team } from './components/Team'
 import { Testimonials } from './components/Testimonials'
-import { BlogPreview } from './components/BlogPreview'
-import { BigCTA } from './components/BigCTA'
-import { Footer } from './components/Footer'
 
 function App() {
   return (
@@ -23,7 +18,6 @@ function App() {
         <About />
         <Strategies />
         <Features />
-        
         <Team />
         <Testimonials />
       </main>
